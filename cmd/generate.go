68c1cc599a6aedca329799be8aae0b5091c294ac
buzzsurfr <buzzsurfr@users.noreply.github.com>
2024-09-15 20:23:08 -0400

    initial copy from POC

---
+++ cmd/generate.go
@@ -0,0 +1,141 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/browserutils/kooky"
+	_ "github.com/browserutils/kooky/browser/all" // register cookie store finders!
+	"github.com/spectrocloud/palette-sdk-go/api/models"
+	"github.com/spectrocloud/palette-sdk-go/client"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+// generateCmd represents the generate command
+var generateCmd = &cobra.Command{
+	Use:     "generate",
+	Aliases: []string{"gen"},
+	Short:   "Generate a kubeconfig file from available Palette clusters.",
+	Long: `Generate (pier gen) will output a kubeconfig file based on the
+available Palette clusters. These will be named according to the project
+and cluster name in Palette.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		host := os.Getenv("PALETTE_HOST")
+		apiKey := os.Getenv("PALETTE_API_KEY")
+		token := os.Getenv("PALETTE_TOKEN")
+
+		if host == "" {
+			host = "api.spectrocloud.com"
+		}
+
+		// If a token is not provided, check browsers for an Authorization cookie.
+		// Will require elevated permissions to access browser cookies.
+		if apiKey == "" && token == "" {
+			cookies := kooky.ReadCookies(kooky.DomainHasSuffix(fmt.Sprintf(".%s", host)), kooky.Name(`Authorization`))
+			if len(cookies) == 0 {
+				fmt.Println("No Authorization cookie found.")
+				os.Exit(1)
+			}
+			token = cookies[0].Value
+		}
+
+		// Create kubeconfig file
+		kubeconfig := clientcmdapi.NewConfig()
+
+		// Initialize a Palette client
+
+		c := client.New(
+			client.WithPaletteURI(host),
+			client.WithAPIKey(apiKey),
+			client.WithJWT(token),
+			client.WithScopeTenant(),
+		)
+
+		// List available projects
+		projects, err := c.GetProjects()
+		if err != nil {
+			panic(err)
+		}
+		if len(projects.Items) == 0 {
+			fmt.Println("\nNo projects found.")
+			return
+		}
+
+		// List clusters in each project
+		for _, project := range projects.Items {
+
+			// Set project scope in client
+			client.WithScopeProject(project.Metadata.UID)(c)
+
+			// Search for clusters
+			clusters, err := c.SearchClusterSummaries(&models.V1SearchFilterSpec{}, []*models.V1SearchFilterSortSpec{})
+			if err != nil {
+				panic(err)
+			}
+
+			// Display the results
+
+			// fmt.Printf("\nFound %d cluster(s):\n", len(clusters))
+			for _, cluster := range clusters {
+				// TODO: add debug log statement for each cluster
+				// fmt.Printf("%s_%s\n", cluster.SpecSummary.ProjectMeta.Name, cluster.Metadata.Name)
+
+				// Fetch kubeconfig from palette
+				clusterKubeconfigData, err := c.GetClusterKubeConfig(cluster.Metadata.UID)
+				if err != nil {
+					panic(err)
+				}
+				if clusterKubeconfigData == "" {
+					continue
+				}
+
+				// Parse into kubeconfig struct
+				clusterKubeconfig, err := clientcmd.Load([]byte(clusterKubeconfigData))
+				if err != nil {
+					panic(err)
+				}
+
+				// Generate name/prefix
+				generatedName := fmt.Sprintf("%s_%s", cluster.SpecSummary.ProjectMeta.Name, cluster.Metadata.Name)
+
+				// Rename cluster and user objects to include project and cluster name
+				kubeconfig.Clusters[generatedName] = clusterKubeconfig.Clusters["kubernetes"].DeepCopy()
+				kubeconfig.AuthInfos[generatedName] = clusterKubeconfig.AuthInfos["kubernetes-admin"].DeepCopy()
+
+				// Create context using generated names
+				context := clusterKubeconfig.Contexts["kubernetes-admin@kubernetes"].DeepCopy()
+				// kubeconfig.Contexts[generatedName] = clusterKubeconfig.Contexts["kubernetes-admin@kubernetes"].DeepCopy()
+				context.Cluster = generatedName
+				context.AuthInfo = generatedName
+
+				kubeconfig.Contexts[generatedName] = context
+			}
+		}
+
+		// Export kubeconfig to stdout
+		kubeconfigBytes, err := clientcmd.Write(*kubeconfig)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(kubeconfigBytes))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(generateCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// generateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// generateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ go.mod
@@ -0,0 +1,77 @@
+module github.com/buzzsurfr/pier
+
+go 1.22.5
+
+require (
+	github.com/browserutils/kooky v0.2.2
+	github.com/spectrocloud/palette-sdk-go v0.0.0-20240909210523-d5dfc0c45e81
+	github.com/spf13/cobra v1.8.1
+	k8s.io/client-go v0.31.1
+)
+
+require (
+	github.com/Velocidex/json v0.0.0-20220224052537-92f3c0326e5a // indirect
+	github.com/Velocidex/ordereddict v0.0.0-20230909174157-2aa49cc5d11d // indirect
+	github.com/Velocidex/yaml/v2 v2.2.8 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-openapi/analysis v0.23.0 // indirect
+	github.com/go-openapi/errors v0.22.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/loads v0.22.0 // indirect
+	github.com/go-openapi/runtime v0.28.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/strfmt v0.23.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-openapi/validate v0.24.0 // indirect
+	github.com/go-sqlite/sqlite3 v0.0.0-20180313105335-53dd8e640ee7 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/gonuts/binary v0.2.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/keybase/go-keychain v0.0.0-20231219164618-57a3676c3af6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	github.com/zalando/go-keyring v0.2.5 // indirect
+	go.mongodb.org/mongo-driver v1.16.0 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/term v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apimachinery v0.31.1 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+	www.velocidex.com/golang/go-ese v0.2.0 // indirect
+)
